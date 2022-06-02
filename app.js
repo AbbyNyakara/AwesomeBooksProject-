f679ec83afb3377858eaeed9244f2801291d669c
aster-alemu <leaster2014w@gmail.com>
2022-06-02 08:08:39 -0700

    Fix eslint errors

--- app.js
+++ app.js
@@ -59,7 +59,9 @@ addButton.addEventListener('click', () => {
   localStorage.setItem('books', JSON.stringify(books));
 });
 
-const today = new Date().toLocaleDateString('en-us', {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'});
+const today = new Date().toLocaleDateString('en-us', {
+  weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'
+});
 
 currentDate.innerHTML = `
   <p>${today}</p>
